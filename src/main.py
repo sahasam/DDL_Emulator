adcf48f1ae1859475f7fec657f2eecf037ab1868
SumanthPal <sumanth@pallamreddy.com>
2025-07-17 12:13:47 -0700

    added multithreading and multiple servers to be spun in one line

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ from hermes.sim.Sim import Sim
 
 import argparse
 import yaml
+from multiprocessing import Process
 
 def load_config(config_file):
     """Load and parse a YAML configuration file. """
@@ -16,14 +17,33 @@ def load_config(config_file):
     except FileNotFoundError:
         print(f"Configuration file {config_file} not found")
         return None
-
+    
+def run_sim_from_config(path: str):
+    """Single thread simulation run from a configuration file."""
+    config = load_config(path)
+    sim = Sim.from_config(config)
+    asyncio.run(sim.run())
+        
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Alternating Bit Protocol Implementation')
-    parser.add_argument('--config_file', '-c', type=str, help='Path to the YAML configuration file')
+    parser.add_argument(
+        '--config_files', '-c',
+        nargs='+',
+        type=str,
+        help="Paths to YAML configuration files (space-separated.)"
+    )
+    
     args = parser.parse_args()
 
-    if args.config_file:
-        config = load_config(args.config_file)
-        sim = Sim.from_config(config)
-        asyncio.run(sim.run())
+    processes = []
+    for path in args.config_files:
+        p = Process(target=run_sim_from_config, args=(path,))
+        p.start()
+        print(f"Started simulation process for {path} with PID {p.pid}")
+        processes.append(p)
+    
+    for p in processes:
+        p.join()
+        print(f"Simulation process for {p.pid} has finished.")
+        
         
